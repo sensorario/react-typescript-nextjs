a5b29dfc1f1cebcb38e69421f2173afeb794dde3
Simone Gentili <sensorario@gmail.com>
2023-12-29 16:37:59 +0100

    feat: add dynamic route sample code

---
+++ next.js/dynamic-routes/app/blog/[slug]/page.tsx
@@ -0,0 +1,3 @@
+export default function PageSlug({ params }) {
+  return <h1>Page Slug ({params.slug})</h1>;
+}
---
+++ next.js/dynamic-routes/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <h1>Home page</h1>
+      </div>
+    </main>
+  )
+}
