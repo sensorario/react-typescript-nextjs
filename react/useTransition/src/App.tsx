56a4548280b6e107a7b9238bc4408c22c4b81486
Simone Gentili <simone.gentili@gcodestorm.it>
2024-03-24 15:56:33 +0100

    feat: add two page contents

--- react/useTransition/src/App.tsx
+++ react/useTransition/src/App.tsx
@@ -1,9 +1,33 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [page, setPage] = useState("/");
+
   return (
     <>
       <h1>useTransaction()</h1>
+
+      <button onClick={() => setPage("/")}>home</button>
+      <button onClick={() => setPage("/blog")}>blog</button>
+
+      {page === "/" && <HomePage />}
+      {page === "/blog" && <BlogPage />}
+    </>
+  );
+}
+
+function HomePage() {
+  return (
+    <>
+      <h2>Homepage</h2>
+    </>
+  );
+}
+
+function BlogPage() {
+  return (
+    <>
+      <h2>Blog!</h2>
     </>
   );
 }
