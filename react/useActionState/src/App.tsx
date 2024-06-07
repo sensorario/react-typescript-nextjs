5a532c5d8f9eae9b548fe4bcf856428bde3a3ba7
Simone Gentili <sensorario@gmail.com>
2024-06-08 01:36:24 +0200

    export css in an-external and sharable design system

--- react/useActionState/src/App.tsx
+++ react/useActionState/src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
 import Componente from "./components";
+import "sensorario-design-system/style/index.css";
 
 function App() {
   return (
-    <>
+    <div className="sensorario-container">
       <h1>useActionState()</h1>
       <Componente />
-    </>
+    </div>
   );
 }
 
