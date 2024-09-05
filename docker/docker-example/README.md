fd6d183e5305308e4325d81eb42c7dc342bc1fcb
Simone Gentili <sensorario@gmail.com>
2024-09-05 14:08:41 +0200

    [Docker]: add basic example

---
+++ docker/docker-example/README.md
@@ -0,0 +1,7 @@
+# Docker example
+
+Grazie al makefile basta lanciare il comando `make`. Questo eseguirà `docker compose up`. A quel punto basta aprire il link http://localhost:8080.
+
+```
+make
+```
---
+++ docker/docker-example/vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+  },
+  plugins: [react()],
+});
