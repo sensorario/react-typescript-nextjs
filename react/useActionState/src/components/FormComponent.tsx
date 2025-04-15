33bff637ad3b7c1992a793970fa900dea85a639a
Simone Gentili <sensorario@gmail.com>
2025-04-15 13:45:50 +0200

    feat(useActionState): complete example

--- react/useActionState/src/components/FormComponent.tsx
+++ react/useActionState/src/components/FormComponent.tsx
@@ -10,9 +10,18 @@ async function handler(previousState: number, data: FormData) {
 export function FormComponent() {
     const [state, formActionHandler] = useActionState(handler, 0);
 
+    const message = (() => {
+        if (state === 10) return "Insane";
+        if (state > 6) return "Mind blowing";
+        if (state > 4) return "Incredible";
+        if (state > 0) return "Brilliant";
+        return "";
+    })();
+
     return (
         <>
             <div className="state">{state}</div>
+            <div className="message">{message}</div>
             <form action={formActionHandler} method="post">
                 <button>Submit</button>
             </form>
