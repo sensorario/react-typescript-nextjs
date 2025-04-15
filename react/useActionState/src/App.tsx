1d66e5fcdf0bc38336e1ef427b67f48b8c8f2850
Simone Gentili <sensorario@gmail.com>
2025-04-15 13:29:54 +0200

    feat(useActionState): complete useActionState example

--- react/useActionState/src/App.tsx
+++ react/useActionState/src/App.tsx
@@ -1,9 +1,11 @@
 import "sensorario-design-system/style/index.css";
+import { FormComponent } from "./components/FormComponent";
 
 function App() {
   return (
     <div className="sensorario-container light">
       <h1>useActionState()</h1>
+      <FormComponent />
     </div>
   )
 }
