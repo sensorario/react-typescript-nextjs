4fe24f3038bd6294b4b5bfc0b4cf57f9a8d46f91
Simone Gentili <sensorario@gmail.com>
2025-04-17 15:21:04 +0200

    feat(design-system): add missing example of react icons

---
+++ design-system/react-icons/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { WiDaySunny } from 'react-icons/wi'
+import "sensorario-design-system/style/index.css";
+
+function App() {
+  return (
+    <div className="sensorario-container light">
+      <h1>React Icons</h1>
+      <WiDaySunny />
+    </div>
+  )
+}
+
+export default App
